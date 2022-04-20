4f2f98ea834d8f13ee694ff02f5520cefc769b98
Joshua Haydon-Guppy <102221047+IHaveBadCode@users.noreply.github.com>
2022-04-20 08:30:57 +0000

    added class code

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 import json, random
+
+class PlayingCards():
+  def __init__(self,Deck):
+    self.Deck = Deck
+
+  def GetCard(self):
+    Card = self.Deck[0]
+    self.Deck.remove(Card)
+    return Card
+    
+p1 = PlayingCards([1,2,3,4])
+
+class Player():
+  def __init__(self):
+    self.hand = []
+    
+  def DrawCard(self):
+    self.hand.append(p1.GetCard())
+    
+
+p2 = Player()
+p2.DrawCard()
+    
